5f3de336b3343e0545cdd88d8c5627c0515c9800
tomasbrown1 <tombrown1@gmail.com>
2015-05-27 12:38:54 +0100

    added gallery and js tag sorting

--- js/js.js
+++ js/js.js
@@ -1,15 +1,60 @@
 $(function() {
 	
-	var timeout;
-	var $text = $('#text');
+	var timeout;												//declare variables
+	var $container = $('#container');
+	var $content = $('#content');
+	var $imgs = $('#gallery img');
+	var $buttons = $('#buttons');
+	var tagged = {};											//finish variables
 	
-	$text.hide();
-	
-	$('#content').append('<img src="img/logo.png" id="logo">');
-		
+	$content.addClass('hide')									//start splash
+	$container.append('<img src="img/logo.png" id="logo">');
 	timeout = setTimeout(function() {
 		$('#logo').fadeOut(500);
 			
-		$text.delay(500).fadeIn(500);
-	}, 1000);
+		$content.delay(500).fadeIn(500);
+	}, 1000);													//end splash
+	
+	$imgs.each(function() {										//start processing tags
+		var img = this;
+		var tags = $(this).data('tags');
+		
+		if (tags) {
+			tags.split(',').forEach(function(tagName){
+				if (tagged[tagName] == null) {
+					tagged[tagName] = [];
+				}
+				tagged[tagName].push(img);
+			});
+		}
+	});															//end processing tags
+	
+	$('<button/>', {											//show all button
+		text: 'Show All',
+		class: 'active',
+		click: function() {
+			$(this)
+				.addClass('active')
+				.siblings()
+				.removeClass('active');
+			$imgs.show();
+		}
+	}).appendTo($buttons);										//show all button
+	
+	$.each(tagged, function(tagName) {							//filter buttons
+		$('<button/>', {
+			text: tagName + ' (' + tagged[tagName].length + ')',
+			click: function() {
+				$(this)
+					.addClass('active')
+					.siblings()
+					.removeClass('active');
+				$imgs
+					.hide()
+					.filter(tagged[tagName])
+					.show();
+			}
+		}).appendTo($buttons);
+	});															//filter buttons
+
 });
